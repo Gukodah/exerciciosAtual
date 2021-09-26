55a90028bf4e9354823ab457bd70568ab3bdaec2
Gustavo Oliveira dos Santos Coelho <oficialgukodah@gmail.com>
2021-09-26 13:54:50 -0300

    Add files via upload

---
+++ Exercicio1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ex1
+{
+    class Exercicio1
+    {
+        public string reverse(string word)
+        {
+            var watch = Stopwatch.StartNew();
+
+            string reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            watch.Stop();
+            Console.WriteLine($"Time tooked: {watch.ElapsedMilliseconds} ms");
+
+            return reversed;
+
+        }
+    }
+}
